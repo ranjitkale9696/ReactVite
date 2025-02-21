7969d9fc690ae934e281da5d9a0be9a7e109ec3a
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-02-21 21:38:24 +0530

    FeatureSection

--- demo1/src/App.jsx
+++ demo1/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import FeatureSection from './components/FeatureSection'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 
@@ -10,6 +11,7 @@ function App() {
    <Navbar/>
    <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <FeatureSection/>
         
       </div>
    </> 
