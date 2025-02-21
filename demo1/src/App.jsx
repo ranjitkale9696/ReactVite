e7c163ae5212674587fc6ef7e83491869599254c
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-02-21 15:06:13 +0530

    tailwind css install
    command step by step ;-
    
    npm install -D tailwindcss@3 postcss autoprefixer
    npx tailwindcss init -p

--- demo1/src/App.jsx
+++ demo1/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <>
-      <p>Welcome to Demo 1 Project</p>
-   </>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
