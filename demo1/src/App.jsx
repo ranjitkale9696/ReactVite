c477b92cec1d221de168e41eea1fe3c2c8ccbcdc
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-02-21 21:24:14 +0530

    Navbar

--- demo1/src/App.jsx
+++ demo1/src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
+   <>
+
+   <Navbar/>
+   <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+   </> 
+    
   )
 }
+
+// const App = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="max-w-7xl mx-auto pt-20 px-6">
+       
+//       </div>
+//     </>
+//   );
+// };
 
 export default App
