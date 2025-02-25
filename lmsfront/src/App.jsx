5bc3ab5dc9f81d46527d0b637180062baea585b2
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-02-25 19:33:16 +0530

    HomePage

--- lmsfront/src/App.jsx
+++ lmsfront/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './components/Home'
 import Header from './components/layout/Header'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     
     <>
     <Header/>
-    <h1 className="text-3xl font-bold underline">welcome to lms project</h1>
+    <Home/>
     
     </>  
   )
